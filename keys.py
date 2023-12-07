eaa8cc48c1f95f16ab2554551376e475133abb47
Dave Currier <dave@vtldesign.com>
2023-12-07 10:58:48 -0500

    adds keyword url checker

--- keys.py
+++ keys.py
@@ -32,8 +32,12 @@ def colored(text, color):
     reset = "\033[0m"  # Resets the color to default
     return f"{colors.get(color, reset)}{text}{reset}"
 
+# Function to convert a keyword into a hyphenated format
+def convert_to_hyphenated(keyword):
+    return keyword.replace(' ', '-').lower()
+
 # File paths for input and output
-input_file_path = 'h1keywords.xlsx'
+input_file_path = 'rank.xlsx'
 output_file_path = 'output.csv'
 
 # Selenium WebDriver configuration for headless Chrome
@@ -68,17 +72,24 @@ for i, row in enumerate(ws.iter_rows(min_row=2, values_only=True)):
     keyword = row[keyword_column]
     url = row[url_column]
 
-    driver.get(url)  # Open the URL with Selenium WebDriver
+    # Convert keyword to hyphenated format
+    hyphenated_keyword = convert_to_hyphenated(keyword)
+
+    # Open the URL with Selenium WebDriver
+    driver.get(url)
     time.sleep(2)  # Wait for the page to load
 
     # Check if the keyword is in the H1 tag of the page
     try:
         h1_tag = driver.find_element(By.TAG_NAME, 'h1').text
-        pass_fail = 'Pass' if keyword.lower() in h1_tag.lower() else 'Fail'
+        keyword_in_h1 = 'Pass' if keyword.lower() in h1_tag.lower() else 'Fail'
     except:
-        pass_fail = 'Fail'  # Fail if H1 tag is not found or other errors
+        keyword_in_h1 = 'Fail'  # Fail if H1 tag is not found or other errors
 
-    results.append((url, keyword, pass_fail))  # Append result to list
+    # Check if the hyphenated keyword is in the URL
+    keyword_in_url = 'Pass' if hyphenated_keyword in url.lower() else 'Fail'
+
+    results.append((url, keyword, keyword_in_h1, keyword_in_url))  # Append result to list
 
     processed_urls += 1
     update_progress_bar(total_urls, processed_urls)  # Update the progress bar
@@ -89,20 +100,12 @@ completion_message = colored("Processing Complete! ", "magenta")
 path_message = colored(f"Your document is saved at {output_file_full_path}", "white")
 print('\n' + completion_message + path_message)
 
-# Group results by URL
-grouped_results = {}
-for url, keyword, pass_fail in results:
-    if url not in grouped_results:
-        grouped_results[url] = []
-    grouped_results[url].append((keyword, pass_fail))
-
 # Write results to a CSV file
 with open(output_file_path, 'w', newline='', encoding='utf-8') as csvfile:
     writer = csv.writer(csvfile)
-    writer.writerow(['URL', 'Keyword', 'Pass/Fail'])
-    for url, checks in grouped_results.items():
-        for keyword, pass_fail in checks:
-            writer.writerow([url, keyword, pass_fail])
+    writer.writerow(['URL', 'Keyword', 'Keyword In H1', 'Keyword in URL'])
+    for url, keyword, keyword_in_h1, keyword_in_url in results:
+        writer.writerow([url, keyword, keyword_in_h1, keyword_in_url])
 
 # Quit the WebDriver
 driver.quit()
